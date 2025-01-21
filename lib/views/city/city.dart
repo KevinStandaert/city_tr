d8bb9d9ab210a624c80d0abd77f6ff50c41fb4c7
kevin standaert <ks.standaert@gmail.com>
2025-01-21 16:15:01 +0100

    feat: creation Trip

--- lib/views/city/city.dart
+++ lib/views/city/city.dart
@@ -1,4 +1,5 @@
 import 'package:city_tr/models/activity.model.dart';
+import 'package:city_tr/models/trip.model.dart';
 import 'package:city_tr/views/city/widgets/activity_card.dart';
 import 'package:flutter/material.dart';
 import '../../data/data.dart' as data;
@@ -12,6 +13,8 @@ class City extends StatefulWidget {
 }
 
 class _CityState extends State<City> {
+  Trip mytrip = Trip(activities: [], city: 'Paris', date: DateTime.now());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +26,52 @@ class _CityState extends State<City> {
         ],
       ),
       body: Container(
-        padding: EdgeInsets.all(5),
-        child: GridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 2,
-          crossAxisSpacing: 2,
-          children: widget.activities
-              .map((activity) => ActivityCard(activity: activity))
-              .toList(),
+        child: Column(
+          children: [
+            Container(
+                padding: EdgeInsets.all(10),
+                height: 300,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text('Choisissez une date'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Selectionner une date'),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text('Choisissez une ville'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Selectionner une ville'),
+                        ),
+                      ],
+                    )
+                  ],
+                )),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 2,
+                  crossAxisSpacing: 2,
+                  children: widget.activities
+                      .map((activity) => ActivityCard(activity: activity))
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
